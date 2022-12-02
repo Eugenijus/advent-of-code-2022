6223158d7a28612fb6df008cc3d1ef713cab32ee
Eugenijus Sabaliauskas <eugenijus@zenitech.co.uk>
2022-12-02 02:38:50 +0200

    day 1 solved
    day 1 solved

---
+++ day1/day1.js
@@ -0,0 +1,50 @@
+const events = require('events');
+const fs = require('fs');
+const readline = require('readline');
+
+async function readLines(stream) {
+  const rl = readline.createInterface({
+    input: stream,
+    crlfDelay: Infinity,
+  });
+  return new Promise((resolve) => {
+    stream.once('error', (_) => resolve(null));
+    const lines = [];
+    rl.on('line', (line) => lines.push(line));
+    rl.on('close', (_) => resolve(lines));
+  });
+}
+
+async function countMaxCalories() {
+  const lines = await readLines(fs.createReadStream('input.txt'));
+  let max1 = 0;
+  let max2 = 0;
+  let max3 = 0;
+  let count = 0;
+  lines.forEach((line) => {
+    if (line === '') {
+      count = 0;
+    } else {
+      count += new Number(line);
+    }
+    if (count > max1) {
+      if (max2 > 0) {
+        max3 = max2;
+        max2 = max1;
+      }
+      max1 = count;
+    } else if (count > max2) {
+      max2 = count;
+    } else if (count > max3) {
+      max3 = count;
+    }
+  });
+  console.log('max1: ', max1);
+  console.log('max2: ', max2);
+  console.log('max3: ', max3);
+  const answer = max1 + max2 + max3;
+  console.log('answer 1: ', max1);
+  console.log('answer 2: ', answer);
+}
+
+countMaxCalories();
